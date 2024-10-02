e64ff32ed39d9f2786605513a7885e054795e83c
Matt Grych <james.grych@gmail.com>
2024-10-02 12:24:16 -0500

    Library Update 1
    -fixed up some of the buttons and created an input form to get data for the new books
    -made it to where the window pops up when you click Add Boook to Library button
    -form will close when the "x" is clicked
    -still need to figure out how to store the data and then display it

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const book = document.getElementById(book);
-const newBook = document.getElementById(newBook);
+
+const addNewBook = document.getElementById("addNewBook");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const bookForm = document.getElementById("bookForm")
+const submitBook = document.getElementById("submitBook")
+const close = document.getElementById("close")
+
+addNewBook.addEventListener("click", () => {
+    bookForm.classList.add("open")
+})
+
+close.addEventListener("click", () => {
+    bookForm.classList.remove("open")
+})
 
 const library = [];
 
-function book(title, author, pages) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
 function newBook() {
+    
     
 }
