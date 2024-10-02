5a3c3c72c0625c8715fec8287d3d0386260b2af4
Matt Grych <james.grych@gmail.com>
2024-10-02 09:16:10 -0500

    Library beginnings
    -Got down the html boilerplate as well as some basic layout for the Library
    -Put everything into a grid for organization
    -Connected everything to the html file
    -Got constructor function written in the js file

---
+++ script.js
@@ -0,0 +1,14 @@
+const book = document.getElementById(book);
+const newBook = document.getElementById(newBook);
+
+const library = [];
+
+function book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+function newBook() {
+    
+}
